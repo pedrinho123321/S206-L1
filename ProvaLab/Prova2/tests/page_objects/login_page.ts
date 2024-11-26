b79c7b12fbcb279be0cb092ced1a0f140938995b
João Marcos Cassemiro <joaomarcos.jm@ges.inatel.br>
2024-11-25 23:36:28 -0300

    Adicionando teste e finalizando a prova

--- ProvaLab/Prova2/tests/page_objects/login_page.ts
+++ ProvaLab/Prova2/tests/page_objects/login_page.ts
@@ -8,7 +8,6 @@ export default class LoginPage {
     private readonly btn_login: Locator;
     private readonly btn_cart: Locator;
 
-
     constructor(page: Page) {
         this.page = page;
 
@@ -28,12 +27,15 @@ export default class LoginPage {
     }
 
     async expectSuccessfullLogin(title: string) {
-        await expect(this.page.locator('[data-test="title"]')).toContainText(title);
+        await expect(this.page.locator('[data-test="title"]')).toContainText(
+            title
+        );
     }
 
     async expectErrorLogin(message: string) {
-        await expect(this.page.locator('[data-test="error"]')).toContainText(message);
-
+        await expect(this.page.locator('[data-test="error"]')).toContainText(
+            message
+        );
     }
 
     async visit(url: string) {
@@ -41,26 +43,59 @@ export default class LoginPage {
     }
 
     async gotoCartPage() {
-      await this.btn_cart.click();
+        await this.btn_cart.click();
     }
 
-    async addProcuct(){
-        await this.page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
-        await this.page.locator('[data-test="add-to-cart-sauce-labs-fleece-jacket"]').click();
-        await this.page.locator('[data-test="add-to-cart-sauce-labs-bolt-t-shirt"]').click();
-        await this.page.locator('[data-test="add-to-cart-sauce-labs-bike-light"]').click();
-        await this.page.locator('[data-test="add-to-cart-sauce-labs-onesie"]').click();
-        await this.page.locator('[data-test="add-to-cart-test\\.allthethings\\(\\)-t-shirt-\\(red\\)"]').click();
+    async addProcuct() {
+        await this.page
+            .locator('[data-test="add-to-cart-sauce-labs-backpack"]')
+            .click();
+        await this.page
+            .locator('[data-test="add-to-cart-sauce-labs-fleece-jacket"]')
+            .click();
+        await this.page
+            .locator('[data-test="add-to-cart-sauce-labs-bolt-t-shirt"]')
+            .click();
+        await this.page
+            .locator('[data-test="add-to-cart-sauce-labs-bike-light"]')
+            .click();
+        await this.page
+            .locator('[data-test="add-to-cart-sauce-labs-onesie"]')
+            .click();
+        await this.page
+            .locator(
+                '[data-test="add-to-cart-test\\.allthethings\\(\\)-t-shirt-\\(red\\)"]'
+            )
+            .click();
     }
 
-    async expectViewCart(){
-        await this.page.getByText('1Sauce Labs Backpackcarry.').click();
-        await this.page.getByText('1Sauce Labs Bike LightA red').click();
-        await this.page.getByText('1Sauce Labs Bolt T-ShirtGet').click();
-        await this.page.getByText('1Sauce Labs Fleece JacketIt\'s').click();
-        await this.page.getByText('1Test.allTheThings() T-Shirt').click();
-        await this.page.getByText('1Sauce Labs OnesieRib snap').click();
+    async expectViewCart() {
+        await this.page.getByText("1Sauce Labs Backpackcarry.").click();
+        await this.page.getByText("1Sauce Labs Bike LightA red").click();
+        await this.page.getByText("1Sauce Labs Bolt T-ShirtGet").click();
+        await this.page.getByText("1Sauce Labs Fleece JacketIt's").click();
+        await this.page.getByText("1Test.allTheThings() T-Shirt").click();
+        await this.page.getByText("1Sauce Labs OnesieRib snap").click();
+    }
+
+    async expectViewImg() {
+        await expect(
+            this.page.locator('[data-test="item-4-img-link"]')
+        ).toBeVisible();
+        await expect(
+            this.page.locator('[data-test="item-0-img-link"]')
+        ).toBeVisible();
+        await expect(
+            this.page.locator('[data-test="item-1-img-link"]')
+        ).toBeVisible();
+        await expect(
+            this.page.locator('[data-test="item-5-img-link"]')
+        ).toBeVisible();
+        await expect(
+            this.page.locator('[data-test="item-2-img-link"]')
+        ).toBeVisible();
+        await expect(
+            this.page.locator('[data-test="item-3-img-link"]')
+        ).toBeVisible();
     }
 }
-
-
--- ProvaLab/Prova2/tests/specs/login_page.spec.ts
+++ ProvaLab/Prova2/tests/specs/login_page.spec.ts
@@ -1,25 +1,33 @@
-import { test } from "../fixtures/fixtures.ts"
+import { test } from "../fixtures/fixtures.ts";
 
-test.beforeEach(async ({loginPage}) => {
-    await loginPage.visit('https://www.saucedemo.com');
+test.beforeEach(async ({ loginPage }) => {
+    await loginPage.visit("https://www.saucedemo.com");
 });
 
-test('Tem que fazer o login', async ({ loginPage }) => {
-    await loginPage.fillLoginInfos("standard_user","secret_sauce");
+test("Tem que fazer o login", async ({ loginPage }) => {
+    await loginPage.fillLoginInfos("standard_user", "secret_sauce");
     await loginPage.submitLogin();
-    await loginPage.expectSuccessfullLogin('Products');
-  });
+    await loginPage.expectSuccessfullLogin("Products");
+});
 
-test('Não deve fazer o login', async ({ loginPage }) => {
-    await loginPage.fillLoginInfos("standard_user","secret_sauc");
+test("Não deve fazer o login", async ({ loginPage }) => {
+    await loginPage.fillLoginInfos("standard_user", "secret_sauc");
     await loginPage.submitLogin();
-    await loginPage.expectErrorLogin("Epic sadface: Username and password do not match any user in this service");
-  });
+    await loginPage.expectErrorLogin(
+        "Epic sadface: Username and password do not match any user in this service"
+    );
+});
 
-test('Tem que adicionar item ao carrinho', async ({ loginPage }) => {
-    await loginPage.fillLoginInfos("standard_user","secret_sauce");
+test("Tem que adicionar item ao carrinho", async ({ loginPage }) => {
+    await loginPage.fillLoginInfos("standard_user", "secret_sauce");
     await loginPage.submitLogin();
     await loginPage.addProcuct();
     await loginPage.gotoCartPage();
     await loginPage.expectViewCart();
+});
+
+test("Tem que ver as imagens todas erradas", async ({ loginPage }) => {
+    await loginPage.fillLoginInfos("standard_user", "secret_sauce");
+    await loginPage.submitLogin();
+    await loginPage.expectViewImg();
 });
